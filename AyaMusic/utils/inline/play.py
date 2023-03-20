929fa37841010e017b3a634c57e3284283d70a8c
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-20 22:13:34 +0700

    update play.py

--- AyaMusic/utils/inline/play.py
+++ AyaMusic/utils/inline/play.py
@@ -37,17 +37,17 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text="II", callback_data=f"ADMIN Pause|{chat_id}"
-            )
+            ),
             InlineKeyboardButton(
                 text="▷",
                 callback_data=f"ADMIN Resume|{chat_id}",
-            )
+            ),
             InlineKeyboardButton(
                 text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
-            )
+            ),
             InlineKeyboardButton(
                 text="▢", callback_data=f"ADMIN Stop|{chat_id}"
-            )
+            ),
         ],
         [
             InlineKeyboardButton(text="𝐉𝐚𝐬𝐚 𝐁𝐨𝐭",
